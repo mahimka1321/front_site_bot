2ca0e67ee28852a5749d5bcc31e097e0a3b46541
mahimka1321git config --global user.name mahimka1321 <xakepok7771@mail.ru>
2024-05-11 12:43:21 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import {useEffect } from 'react';
+import { useTelegram } from './hooks/useTelegram';
+
+import Header from './components/header/Header';
+
 import './App.css';
 
 function App() {
+
+  const {tg, onToggelButton} = useTelegram();
+
+  useEffect( () => {
+    tg.ready();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <button onClick={onToggelButton}>toggle</button>
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+import Button from '../button/Button';
+import { useTelegram } from '../../hooks/useTelegram';
+
+
+const Header = () => {
+
+    const { user, onClose } = useTelegram();
+
+  return (
+    <div className={'header'}>
+        <Button onClick={onClose}>Закрыть</Button>
+        <span className={'username'}>
+            {user?.userneme}
+        </span>
+    </div>
+  );
+}
+
+export default Header;
