2fb7f8378cbb146892b5329923a1f8a95b6a3b31
mahimka1321git config --global user.name mahimka1321 <xakepok7771@mail.ru>
2024-05-12 14:44:08 +0300

    init.v.1.11

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ProductList.css'
+import ProductItem from '../productItem/ProductItem';
+import { useTelegram } from '../../hooks/useTelegram';
+
+const products = [
+  {id: '1', title: 'Джинцы', price: 4999, description: 'Синего цвета'},
+  {id: '2', title: 'Джинцы', price: 2499, description: 'Черного цвета'},
+  {id: '3', title: 'Джинцы', price: 5299, description: 'Зеленого цвета'},
+
+]
+
+const getTotalPrice = (items = []) => {
+  return items.reduce((acc, item) => {
+    return acc += item.price
+  }, 0)
+}
 
 const ProductList = () => {
+
+  const [addedItems, setAddedItems] = useState([]);
+  const {tg} = useTelegram();
+  const onAdd = (product) => {
+    const alreadyAdded = addedItems.find(item => item.id === product.id);
+    let newItems = [];
+
+    if(alreadyAdded) {
+      newItems = addedItems.filter(item => item.id !== product.id);
+    } else {
+      newItems = [...addedItems, product];
+    }
+
+    setAddedItems(newItems);
+
+    if(newItems.length === 0) {
+      tg.MainButtom.hide();
+    } else {
+      tg.MainButtom.show();
+      tg.MainButtom.setParams({
+        text: `Купить ${getTotalPrice(newItems)}`
+      })
+    }
+  }
+
   return (
-    <></>
+    <div className={'list'}>
+      {products.map(item => (
+        <ProductItem
+          product={item}
+          onAdd={onAdd}
+          className={'item'}
+          />
+      ))}
+    </div>
   );
 };
 
