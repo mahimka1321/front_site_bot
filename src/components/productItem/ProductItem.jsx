aa6c51136f348d4c22dc24699251dbef7b7745fd
mahimka1321git config --global user.name mahimka1321 <xakepok7771@mail.ru>
2024-05-19 19:08:15 +0300

    init.v.1.16.5

--- src/components/productItem/ProductItem.jsx
+++ src/components/productItem/ProductItem.jsx
@@ -16,9 +16,6 @@ const ProductItem = ({product, className, onAdd, btnText}) => {
   return (
     <div className={'product ' + className}>
         <img className={'img'} src={product.imgUrl} alt="" />
-        <div className={'price'}>
-            <span>{product.price} ₽</span>
-        </div>
         <div className={'title'}>{product.title}<span className={'description'}> / {product.description}</span></div>
         <div className={'container_information-total'}> <p className={'__star'}>★ <span className={'__star_info'}>4.5</span></p> <span className={'__info_shops_item'}> <samp>·</samp>432 оценка</span></div>
         <Button className={'add-btn'} onClick={onAddHandler}>
